64782ec454bf85c5beb2926a741791ee95952516
Lucas Tavares Brito <lucastavaresbrito@outlook.com>
2025-02-20 15:57:52 -0300

    Quartz sync: Feb 20, 2025, 3:57 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -44,12 +44,18 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta()],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
     Component.Darkmode(),
+    Component.MobileOnly(Component.Spacer()),
+    
+  ],
+  right: [
+    Component.Search(),
     Component.Explorer({
       title: "Menu",
       filterFn: (node) => {
@@ -61,5 +67,4 @@ export const defaultListPageLayout: PageLayout = {
       folderDefaultState: "open",
     }),
   ],
-  right: [],
 }
